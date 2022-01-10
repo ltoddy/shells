1940a3a1edb5b54ef74650de30896846c1bf5116
ltoddy <taoliu0509@gmail.com>
2022-01-10 10:07:37 +0800

    feat: add rust doc

--- README.md
+++ README.md
@@ -16,7 +16,7 @@ use std::io;
 use shells::bash;
 
 pub fn main() -> io::Result<()> {
-    let (code, stdout, stderr) = bash!("echo '{} + {}' | bc", 1, 2);
+    let (code, stdout, stderr) = bash!("echo '{} + {}' | bc", 1, 2)?;
     assert_eq!(code, 0);
     assert_eq!(stdout, String::from("3\n"));
     assert_eq!(stderr, String::new());
--- src/lib.rs
+++ src/lib.rs
@@ -4,47 +4,57 @@
 // option. This file may not be copied, modified, or distributed
 // except according to those terms.
 
+//! Execute in the shell
+//! ```toml
+//! [dependencies.shells]
+//! git = "https://github.com/ltoddy/shells"
+//! branch = "master"
+//! ```
+//!
+//! ## Usage
+//!
+//! ```rust
+//! use std::io;
+//!
+//! use shells::bash;
+//!
+//! pub fn main() -> io::Result<()> {
+//!     let (code, stdout, stderr) = bash!("echo '{} + {}' | bc", 1, 2)?;
+//!     assert_eq!(code, 0);
+//!     assert_eq!(stdout, String::from("3\n"));
+//!     assert_eq!(stderr, String::new());
+//!     Ok(())
+//! }
+//! ```
+
 use std::io;
 use std::process::Command;
 
 #[macro_export]
 macro_rules! sh {
-    ($($arg:tt)*) => {
-        {
-            $crate::execute_by("sh", &format!($($arg)*))
-        }
-    };
+    ($($arg:tt)*) => { $crate::execute_by("sh", &format!($($arg)*)) };
 }
 
 #[macro_export]
 macro_rules! bash {
-    ($($arg:tt)*) => {
-        {
-            $crate::execute_by("bash", &format!($($arg)*))
-        }
-    };
+    ($($arg:tt)*) => { $crate::execute_by("bash", &format!($($arg)*)) };
 }
 
 #[macro_export]
 macro_rules! zsh {
-    ($($arg:tt)*) => {
-        {
-            $crate::execute_by("zsh", &format!($($arg)*))
-        }
-    };
+    ($($arg:tt)*) => { $crate::execute_by("zsh", &format!($($arg)*)) };
 }
 
+#[doc(hidden)]
 pub fn execute_by(shell: &str, cmd: &str) -> io::Result<(i32, String, String)> {
-    println!();
-
     let mut command = Command::new(shell);
     command.arg("-c").arg(cmd);
 
-    command.output().and_then(|output| {
+    command.output().map(|output| {
         let code = output.status.code().unwrap_or_else(|| if output.status.success() { 0 } else { -1 });
         let stdout = String::from_utf8_lossy(&output.stdout).into_owned();
         let stderr = String::from_utf8_lossy(&output.stderr).into_owned();
-        Ok((code, stdout, stderr))
+        (code, stdout, stderr)
     })
 }
 
@@ -53,6 +63,28 @@ pub mod test {
     #[test]
     pub fn test_sh() {
         let result = sh!("echo '{} + {}' | bc", 1, 2);
+        assert!(result.is_ok());
+
+        let (code, stdout, stderr) = result.unwrap();
+        assert_eq!(code, 0);
+        assert_eq!(stdout, String::from("3\n"));
+        assert_eq!(stderr, String::new());
+    }
+
+    #[test]
+    pub fn test_bash() {
+        let result = bash!("echo '{} + {}' | bc", 1, 2);
+        assert!(result.is_ok());
+
+        let (code, stdout, stderr) = result.unwrap();
+        assert_eq!(code, 0);
+        assert_eq!(stdout, String::from("3\n"));
+        assert_eq!(stderr, String::new());
+    }
+
+    #[test]
+    pub fn test_zsh() {
+        let result = zsh!("echo '{} + {}' | bc", 1, 2);
         assert!(result.is_ok());
 
         let (code, stdout, stderr) = result.unwrap();
