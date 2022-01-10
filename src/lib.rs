9c972558f8a452f09dc8c51d389a54281a1d500f
liutao.rs <liutao.rs@bytedance.com>
2022-01-10 09:51:02 +0800

    feat: add sh, bash, zsh macros

--- src/lib.rs
+++ src/lib.rs
@@ -4,11 +4,60 @@
 // option. This file may not be copied, modified, or distributed
 // except according to those terms.
 
+use std::io;
+use std::process::Command;
+
+#[macro_export]
+macro_rules! sh {
+    ($($arg:tt)*) => {
+        {
+            $crate::execute_by("sh", &format!($($arg)*))
+        }
+    };
+}
+
+#[macro_export]
+macro_rules! bash {
+    ($($arg:tt)*) => {
+        {
+            $crate::execute_by("bash", &format!($($arg)*))
+        }
+    };
+}
+
+#[macro_export]
+macro_rules! zsh {
+    ($($arg:tt)*) => {
+        {
+            $crate::execute_by("zsh", &format!($($arg)*))
+        }
+    };
+}
+
+pub fn execute_by(shell: &str, cmd: &str) -> io::Result<(i32, String, String)> {
+    println!();
+
+    let mut command = Command::new(shell);
+    command.arg("-c").arg(cmd);
+
+    command.output().and_then(|output| {
+        let code = output.status.code().unwrap_or_else(|| if output.status.success() { 0 } else { -1 });
+        let stdout = String::from_utf8_lossy(&output.stdout).into_owned();
+        let stderr = String::from_utf8_lossy(&output.stderr).into_owned();
+        Ok((code, stdout, stderr))
+    })
+}
+
 #[cfg(test)]
-mod tests {
+pub mod test {
     #[test]
-    fn it_works() {
-        let result = 2 + 2;
-        assert_eq!(result, 4);
+    pub fn test_sh() {
+        let result = sh!("echo '{} + {}' | bc", 1, 2);
+        assert!(result.is_ok());
+
+        let (code, stdout, stderr) = result.unwrap();
+        assert_eq!(code, 0);
+        assert_eq!(stdout, String::from("3\n"));
+        assert_eq!(stderr, String::new());
     }
 }
